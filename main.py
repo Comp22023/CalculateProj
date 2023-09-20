47b064aecc6c20e0e006eb3a12aa2121bd908f8a
RomaDanilDenis <comp12023@mail.ru>
2023-09-20 12:00:40 +0300

    first

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ from tkinter import *
 root = Tk()
 root.geometry("400x400")
 root.title("Калькулятор by 4IP")
+root.configure(bg="white")
+
+label = Label (text="Калькулятор", font='Times 25', fg='black')
+label.pack()
+label.configure(bg="white")
+
+
+
+
 root.mainloop()
